38066fa7bb77125be97c29f2ea4e5000dd8f494a
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2023-04-24 09:53:16 +0200

    Neovim statusline indicator icons.

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -3,6 +3,10 @@ local lsp_status = require('lsp-status')
 lsp_status.config({
     status_symbol = '',
     current_function = false,
+    indicator_errors = '',
+    indicator_warnings = '',
+    indicator_info = '',
+    indicator_hint = '',
     indicator_ok = '',
 })
 
