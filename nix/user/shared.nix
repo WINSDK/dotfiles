0a274d52c8c31f5e16fb8a6b7e6c6a5f206fea6a
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2026-04-18 22:19:48 +0200

    backup

--- nix/user/shared.nix
+++ nix/user/shared.nix
@@ -27,6 +27,7 @@
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "backup";
   home-manager.users.nicolas =
     { config, ... }:
     let
