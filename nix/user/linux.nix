fad75654715a609cc3e7aab125dd0156723edbb2
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2026-04-19 12:41:24 +0200

    groups

--- nix/user/linux.nix
+++ nix/user/linux.nix
@@ -2,6 +2,7 @@
 {
   users.users.nicolas = {
     isNormalUser = true;
+    extraGroups = [ "wheel" "docker" ];
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJe9xDIDvesRTCJL/KY85cyCDcJYsPeUq1mOG4k82Jat nicolas"
